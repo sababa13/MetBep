306ade324a3dc527a08c5b52ae25563406b83579
KatjaFAF172 <ejovaekaterina@gmail.com>
2021-03-18 17:11:34 +0200

    PPL-1 | removed commits

--- models/role.js
+++ models/role.js
@@ -9,34 +9,3 @@ var roleSchema = new Schema(
 
 //Export model
 module.exports = mongoose.model('Role', roleSchema);
-
-// var mongoose = require('mongoose'),
-// 	Schema = mongoose.Schema;
-
-// var roleSchema = new mongoose.Schema({
-// 	_id: Number,
-// 	role_name: String
-// });
-
-// exports.def =  
-// 	{
-// 		"Role":{
-// 			"_id":"Role",
-// 			"required": ["_id", "role_name",],
-// 			"properties":{
-// 				"_id":{
-// 					"type":"integer",
-// 					"format": "int64",
-// 					"description": "An unique identifier",
-// 					"minimum": "0.0",
-// 					"maximum": "1000000.0"
-// 				},
-// 				"role_name":{
-// 					"type":"string",
-// 					"description": "Hospital staff roles"
-// 				}
-// 			}
-// 		}
-// 	};
-
-// exports.model = mongoose.model('roles', roleSchema);
